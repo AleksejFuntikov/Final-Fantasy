0f0b1e4d29469f7682f2559fbdaebaf4432b439d
Aleksej Funtikov <Aleksejfuntikov@gmail.com>
2023-08-22 15:35:45 +0300

    add auth + kabinet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "articles#index"
+  
+  get "profile", to: "profiles#show", as: "profile"
 
   # get "/articles", to: "articles#index"
   #get "/articles/:id", to: "articles#show"
