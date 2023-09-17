0f7dbc496349f9086c0ae229bd2a50048888eec7
AleksejFuntikov <86779779+AleksejFuntikov@users.noreply.github.com>
2023-09-17 18:15:21 +0300

    delete conflicts code (#13)
    Co-authored-by: Aleksej Funtikov <Aleksejfuntikov@gmail.com>

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -2,28 +2,10 @@
 
 module Visible
   extend ActiveSupport::Concern
-<<<<<<< HEAD
-  
-  VALID_STATUSES = ["published", "private", "archived"]
-=======
->>>>>>> ceaf6b173dc917f7527dbe47bc21e197dfd7a170
 
   included do
     enum status: { "published" => "published", "privated" => "privated", "archived" => "archived" }
     scope :public_count, -> { published.count }
   end
-<<<<<<< HEAD
-
-  class_methods do
-      def public_count
-          where(status: :published).count
-      end
-    end
-
-  def archived?
-    status == "archived"
-  end
 end
-=======
-end
->>>>>>> ceaf6b173dc917f7527dbe47bc21e197dfd7a170
+
